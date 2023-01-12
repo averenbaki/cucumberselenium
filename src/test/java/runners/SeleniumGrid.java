98e1fa470f9a2726d4622ed47445d66b9cfbfc99
Eren BAKİ <averenbaki@gmail.com>
2023-01-12 23:00:14 +0300

    cucumberday04-seleniumgrid

---
+++ src/test/java/runners/SeleniumGrid.java
@@ -0,0 +1,28 @@
+package runners;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SeleniumGrid {
+
+   static  WebDriver driver;
+
+    public static void main(String[] args) throws MalformedURLException {
+        DesiredCapabilities cap=new DesiredCapabilities();
+        cap.setPlatform(Platform.WIN10);
+        cap.setBrowserName("firefox");
+
+        driver = new RemoteWebDriver(new URL("http://192.168.1.13:4444/"),cap);
+
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        driver.quit();
+    }
+
+}
